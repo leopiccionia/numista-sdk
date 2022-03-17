45037d814f1a498d24a9f6bdf3f09728442d8ca2
Leonardo <leopiccionia@gmail.com>
2022-03-17 16:33:36 -0300

    Experimental Stream API

--- src/pagination.ts
+++ src/pagination.ts
@@ -43,17 +43,39 @@ export class PaginatedResult<Req extends PaginatedRequest, Res extends Paginated
     return (this.#params.count * this.#page) < this.#count
   }
 
-  async next (): Promise<void> {
+  async next (): Promise<Res> {
     const nextPage = this.#page + 1
     const params: Req = { ...this.#params, page: nextPage }
 
-    const { count, ...data } = await this.#rest.request<Res>('GET', this.#url, params, null, this.#isAuthenticated)
+    const res = await this.#rest.request<Res>('GET', this.#url, params, null, this.#isAuthenticated)
+    const { count, ...data } = res
     this.#count = count
     this.#data = mergeObjects(this.#data, data)
     this.#page = nextPage
+    return res
   }
 
   get page (): number {
     return this.#page
+  }
+
+  stream (): ReadableStream<Omit<Res, 'count'>> {
+    let canceled = false
+
+    return new ReadableStream<Omit<Res, 'count'>>({
+      cancel: () => {
+        canceled = true
+      },
+      start: async (controller) => {
+        controller.enqueue(this.#data)
+
+        while (this.hasNext() && !canceled) {
+          const { count, ...data } = await this.next()
+          controller.enqueue(deepClone(data))
+        }
+
+        controller.close()
+      },
+    })
   }
 }
